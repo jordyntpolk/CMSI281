8907f708805cdf60a53cb3cd52bfe333811bd1b7
jordyntpolk <35432728+jordyntpolk@users.noreply.github.com>
2018-09-27 13:12:30 -0700

    Create IntLinkedList.java

---
+++ Classwork/Classwork05/IntLinkedList.java
@@ -0,0 +1,110 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  IntLinkedList.java
+ *  Purpose       :  To practice working with Nodes
+ *  @author       :  Jordyn Polk
+ *  Date written  :  2018-09-27
+ *  Description   :  This implements the insertAt() which takes an integer index location and a data value
+ *                   and inserts a node with that data value BEFORE the node at that index. The removeAt()
+ *                   method takes an integer index location ONLY, and removes the noce at that index.
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the input arguments are not allowed
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2018-09-27  Jordyn Polk   Initial set up
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+public class IntLinkedList {
+
+      private Node node;
+      private int  size;
+
+     // the constructor
+      IntLinkedList() {
+         node = null;
+         size = 0;
+      }
+
+      public int getSize() {
+         return size;
+      }
+
+      public void prepend( int dataToAdd ) {
+         Node currentnode = node;
+         node = new Node( dataToAdd );
+         node.next = currentnode;
+         size++;
+      }
+
+      private class Node {
+
+         int data;            // remember this is an IntLinkedList
+         Node next;           // here's the self-referential part
+
+         // constructor
+         Node( int d ) {
+            data = d;
+            next = null;
+         }
+      }
+
+      public Iterator getIteratorAt( int index ) {
+         if( (index > size) || (index < 0) ) {
+            throw new IllegalArgumentException();
+         }
+         Iterator it = new Iterator();
+         while( index > 0 ) {
+            it.next();
+            index--;
+         }
+         return it;
+      }
+
+      public class Iterator {
+         private Node currentNode;
+
+         Iterator() {
+            currentNode = node;
+         }
+
+         public void next() {
+            if( currentNode == null ) {
+               return;
+            } else {
+               currentNode = currentNode.next;
+            }
+         }
+
+         public boolean hasNext() {
+            return ((currentNode != null) && (currentNode.next != null));
+         }
+
+         public int getCurrentInt() {
+            return currentNode.data;
+         }
+      }
+      public void insertAt( int index , int num ) {
+        if(index > size || index < 0){
+          throw new IllegalArgumentException();
+        } else {
+        Iterator it = getIteratorAt( index );
+        Node nodeToAdd = new Node( num );
+        nodeToAdd.next = it.currentNode.next;
+        it.currentNode.next = nodeToAdd;
+      }
+      size++;
+    }
+
+      public void removeAt( int index ) {
+          if ( index > size -1 || index < 0 ) {
+              throw new IllegalArgumentException( "Invalid index." );
+          }
+          Iterator newIt = getIteratorAt(index);
+          Node rem = newIt.currentNode.next;
+          Iterator myIt = getIteratorAt(index-1);
+          myIt.currentNode.next = rem;
+          size--;
+          return rem; // Added return rem to return the value
+          }
+   }
